ab579e1e92d22acbd0f7358b9300beef21f15caf
dmf296 <dmf296@psu.edu>
2024-05-26 15:14:48 -0400

    this commit has the initial versions of create_birdsites_database.py and make_locations_table.py.  admin_functions.py is now the testing module.

---
+++ create_birdsites_database.py
@@ -0,0 +1,19 @@
+'''This module creates the BirdSites database'''
+
+import sqlite3
+
+
+def create_birdsites():
+    '''Create the database if it doesn't exist'''
+    # database name
+    database_name = "BirdSites.db"
+
+    try:
+        conn = sqlite3.connect(database_name)
+        print("Database BirdSites.db created.")
+        conn.close()
+    except sqlite3.Error:
+        # database already exists or some other error occurred
+        print("Database BirdSites.db not created.")
+
+    return
---
+++ make_locations_table.py
@@ -0,0 +1,23 @@
+'''This module will create and test the locations table in the BirdSites database'''
+import sqlite3
+
+
+def make_locations_table():
+    '''this module creates the locations table'''
+    # call sqlite3
+    try:
+        con = sqlite3.connect("BirdSites.db")
+        cur = con.cursor()
+        cur.execute("CREATE TABLE if not exists locations(pk_loc INTEGER PRIMARY KEY,\
+                    loc_full_name TEXT NOT NULL, county_name TEXT, state_name TEXT NOT NULL, geolocation REAL,\
+                    linkToNWSSite TEXT, linkToParkWebSite TEXT, linkToeBirdSite TEXT,\
+                    linkToBirdCastSite TEXT)")
+        con.commit()
+
+    except sqlite3.Error as error:
+        print("Error while connecting to SQLite", error)
+    finally:
+        if con:
+            con.close()
+            print("The SQLite connection is closed")
+    return
