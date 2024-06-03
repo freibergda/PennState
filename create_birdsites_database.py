24f09b97e59af3719aaf8d1a1392d5930b79db71
DA. Freiberg <freibergda@users.noreply.github.com>
2024-06-02 23:04:51 -0400

    Update create_birdsites_database.py

--- create_birdsites_database.py
+++ create_birdsites_database.py
@@ -11,9 +11,13 @@ def create_birdsites():
     try:
         conn = sqlite3.connect(database_name)
         print("Database BirdSites.db created.")
-        conn.close()
     except sqlite3.Error as error:
         # database already exists or some other error occurred
         print(error,"Database BirdSites.db not created.")
-
+   finally: 
+    # This will always execute.  If connection is open, close it 
+        if conn: 
+            # using close() method, close the connection 
+            conn.close() 
+            print("the sqlite connection is closed")
     return
