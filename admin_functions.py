76623010c7971648c6183fcc5928aafec8268d26
dmf296 <dmf296@psu.edu>
2024-06-04 17:50:34 -0400

    Testing for database status

--- admin_functions.py
+++ admin_functions.py
@@ -2,7 +2,8 @@
 import sqlite3
 import streamlit as st
 import create_birdsites_database
-import make_locations_table
+#import make_locations_table
+
 
 def load_locations_table():
     '''Put the data into the database (mimic the adminstrator)'''
@@ -20,12 +21,12 @@ def load_locations_table():
 
 def main():
     '''this is the main module, which will test the admin functions'''
-    create_birdsites_database.create_birdsites()
-    
-    make_locations_table.make_locations_table()
+    database_name = create_birdsites_database.create_birdsites()
+
+    # make_locations_table.make_locations_table()
 
     # mimic the administrator by loading the locations table
-    load_locations_table()
+    # load_locations_table()
 
     # Display the locations table (static, final will be dynamic so that Administrator can create or edit)
     # Set page variables
@@ -35,17 +36,57 @@ def main():
 
     # The following sets up a connection to BirdSites_db using a SQLAlchemy Engine
     # Create the SQL connection as specified in the /.streamlit/secrets.toml file.
-    conn = st.connection('BirdSites_db', type='sql')
 
-    # Query and display the locations data
-    locations = conn.query("select * from locations")
-    st.dataframe(locations)
+    try:
+        # Making a connection between sqlite3
+        # database and Python Program
+        conn = st.connection(database_name, type='sql')
+
+        # If sqlite3 makes a connection with python
+        # program then it will print "Connected to SQLite"
+        # Otherwise it will show errors
+        print("Connected to ", database_name)
+
+        # Getting all tables from sqlite_master
+        sql_query = """SELECT name FROM "+database_name + 
+        " WHERE type='table';"""
+
+        # Creating cursor object using connection object
+        cursor = conn.cursor()
+
+        # executing our sql query
+        cursor.execute(sql_query)
+        print("List of tables\n")
+
+        # printing all tables list
+        print(cursor.fetchall())
+
+    except sqlite3.Error as error:
+        print("Failed to execute the above query", error)
+
+    finally:
+
+        # Inside Finally Block, If connection is
+        # open, we need to close it
+        if conn:
+
+            # using close() method, we will close
+            # the connection
+            conn.close()
+
+            # After closing connection object, we
+            # will print "the sqlite connection is
+            # closed"
+            print("the sqlite connection is closed")
+
+    # st.dataframe(locations)
+    # if starting from blank, there should be no locations table
 
     # Drop the table so that the data loaded by script won't accumulate
-    con = sqlite3.connect("BirdSites.db")
-    con.execute("DROP TABLE locations")
-    con.commit()
-    con.close()
+    # con = sqlite3.connect("BirdSites.db")
+    # con.execute("DROP TABLE locations")
+    # con.commit()
+    # con.close()
 
 
 if __name__ == "__main__":
--- create_birdsites_database.py
+++ create_birdsites_database.py
@@ -1,7 +1,7 @@
 '''This module creates the BirdSites database'''
 
 import sqlite3
-
+from datetime import datetime
 
 def create_birdsites():
     '''Create the database if it doesn't exist'''
@@ -10,14 +10,20 @@ def create_birdsites():
 
     try:
         conn = sqlite3.connect(database_name)
-        print("Database BirdSites.db created.")
+        # Getting current date/time to confirm creation
+        curr_datetime = datetime.now()
+        print("Database ",database_name," created.",curr_datetime)
     except sqlite3.Error as error:
         # database already exists or some other error occurred
-        print(error, "Database BirdSites.db not created.")
+        curr_datetime = datetime.now()
+        print(error, "Database BirdSites.db not created.",curr_datetime )
     finally:
         # This will always execute.  If connection is open, close it
+        curr_datetime = datetime.now()
         if conn:
             # using close() method, close the connection
             conn.close()
-            print("the sqlite connection is closed")
-    return
+            print("the sqlite connection is closed",curr_datetime)
+        else:
+            print(curr_datetime)
+    return database_name
