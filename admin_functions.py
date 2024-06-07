307da7b1c1d7af1600425937bd5bd32ca92b6ebe
dmf296 <dmf296@psu.edu>
2024-06-07 17:26:10 -0400

    Test commit

--- admin_functions.py
+++ admin_functions.py
@@ -2,7 +2,7 @@
 import sqlite3
 import streamlit as st
 import create_birdsites_database
-import make_locations_table
+
 
 def load_locations_table():
     '''Put the data into the database (mimic the adminstrator)'''
@@ -16,17 +16,18 @@ def load_locations_table():
     con.commit()
     con.close()
 
-
 def main():
     '''this is the main module, which will test the admin functions'''
-    create_birdsites_database.create_birdsites()
-    
-    make_locations_table.make_locations_table()
+ 
+    database_name = create_birdsites_database.create_birdsites()
+
+    # make_locations_table.make_locations_table()
 
     # mimic the administrator by loading the locations table
-    load_locations_table()
+    # load_locations_table()
 
-    # Display the locations table (static, final will be dynamic so that Administrator can create or edit)
+    # Display the locations table (static, final will be dynamic so that
+    # Administrator can create or edit)
     # Set page variables
     st.set_page_config(layout="wide")
     st.title("BirdSites Database")
@@ -34,17 +35,28 @@ def main():
 
     # The following sets up a connection to BirdSites_db using a SQLAlchemy Engine
     # Create the SQL connection as specified in the /.streamlit/secrets.toml file.
-    conn = st.connection('BirdSites_db', type='sql')
+    print(database_name)
 
     # Query and display the locations data
-    locations = conn.query("select * from locations")
-    st.dataframe(locations)
+    # locations = conn.query("select * from locations")
+    # st.dataframe(locations)
 
-    # Drop the table so that the data loaded by script won't accumulate
-    con = sqlite3.connect("BirdSites.db")
-    con.execute("DROP TABLE locations")
-    con.commit()
-    con.close()
+    # List the tables in the database
+    # https://docs.streamlit.io/develop/concepts/connections/connecting-to-data
+    conn = st.connection("sql")
+    
+
+    #("""SELECT name FROM sqlite_master  
+    #    WHERE type='table';""")
+    
+    #st.dataframe(table_list)
+    conn.close()
+        # Drop the table so that the data loaded by script won't accumulate
+        # con = sqlite3.connect(database_name)
+        # con.execute("DROP TABLE locations")
+        # con.commit()
+        # con.close()
+    
 
 
 if __name__ == "__main__":
--- create_birdsites_database.py
+++ create_birdsites_database.py
@@ -7,17 +7,24 @@ def create_birdsites():
     '''Create the database if it doesn't exist'''
     # database name
     database_name = "BirdSites.db"
+    # Initialize the connection variable
 
     try:
         conn = sqlite3.connect(database_name)
-        print("Database BirdSites.db created.")
+        print("Database BirdSites.db created or opened.")
+        ######################
+        # just for testing out
+        # list all tables
+        sql_query = """SELECT name FROM sqlite_master  
+        WHERE type='table';"""
+        cursor = conn.cursor()
+        cursor.execute(sql_query)
+        print(cursor.fetchall())
+        #########################
+        conn.close()
+        print("the sqlite connection is closed")
     except sqlite3.Error as error:
         # database already exists or some other error occurred
-        print(error,"Database BirdSites.db not created.")
-   finally: 
-    # This will always execute.  If connection is open, close it 
-        if conn: 
-            # using close() method, close the connection 
-            conn.close() 
-            print("the sqlite connection is closed")
-    return
+        print(error, "Database BirdSites.db not created.")
+
+    return database_name
