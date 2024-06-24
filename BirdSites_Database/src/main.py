c042aac3f809261c0efe20d82a7d9b9eff3f3c52
dmf296 <dmf296@psu.edu>
2024-06-23 22:55:34 -0400

    Updated BirdSites_Database structure display and testsuite

--- BirdSites_Database/src/main.py
+++ BirdSites_Database/src/main.py
@@ -1,13 +1,26 @@
+'''main.py 
+Jira Task S8S4-147 User Story: As an Administrator, I want a BirdSites 
+database created with administrator, locations, groups and locations_groups 
+tables and display verification of tables, so that I can create or modify 
+records that will provide a positive birdwatching user experience.
+Acceptance Criteria:
+Given:  The BirdSites database and its tables do not exist.
+When: The Administrator executes the admin_functions function
+Then: The admin_function (the system) will create the BirdSites database, 
+the administrator_records, locations, groups, and locations_groups tables 
+inside the database and display a list of all tables and the columns in those tables.
+Variables: none
+Parameters: none'''
+
 import streamlit as st
-from display_birdsites_database_tables import display_all_tables
+#from display_birdsites_database_tables import display_all_tables
+import display_birdsites_database_tables
 import create_birdsites_database
 import make_admin_table
 import make_locations_table
 import make_groups_table
 import make_locations_groups_table
 
-#https://discuss.streamlit.io/t/how-to-increase-the-width-of-web-page/7697
-st.set_page_config(layout="wide")
 st.title('BirdSites Database')
 
 def main():
@@ -18,9 +31,9 @@ def main():
     make_groups_table.make_groups_table(database_name)
     make_locations_groups_table.make_locations_groups_table(database_name)
 
-    tables = display_all_tables(database_name)
+    tables = display_birdsites_database_tables.display_all_tables(database_name)
     
-    st.header('List of tables')
+    st.header('List of tables and their columns')
     for (table_name, columns) in tables:
         st.subheader(table_name)
         st.markdown(', '.join(columns))
